576e1e8525f36624b3be1167b53efc64e7198ad2
unknown <julia_trento@saojose.sc.senai.br>
2019-06-17 12:32:06 -0300

    inicio aula 3 modulo 3

---
+++ mod3/aula  3/2/todos.js
@@ -0,0 +1,11 @@
+var listElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement =document.querySelector("#app");
+
+var todos = [
+    'fazer cafe',
+    'beber agua',
+    'jogar lixo no lixo'
+];
+console.log(listElement);
+console.log(inputElement);
